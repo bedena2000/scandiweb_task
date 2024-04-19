757e15f2bd58720443f3df5f94115985f84b8e55
bedena2000 <bedena2000@gmail.com>
2024-04-19 12:48:55 +0400

    done: cardListPage

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,28 @@
 /* eslint-disable react/prop-types */
 import { Component } from "react";
 import styles from "./Card.module.css";
+import addToCartLogo from "../../assets/icons/add_cart_icon.png";
 
 class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isHover: false,
+    };
+  }
+
+
   render() {
+    const addToCardHandler = () => {
+      console.log('add to cart is being activated');
+    };
+    
     return (
-      <div className={`${styles["cardItemWrapper"]}`}>
+      <div
+        onMouseEnter={() => this.setState({ isHover: true })}
+        onMouseLeave={() => this.setState({ isHover: false })}
+        className={`${styles["cardItemWrapper"]}`}
+      >
         <div className={`${styles["cardItemImage"]}`}>
           <img alt="cardItem" src={this.props.cardImage} />
         </div>
@@ -13,6 +30,12 @@ class Card extends Component {
         <p className={`${styles["cardItemName"]}`}>{this.props.cardTitle}</p>
 
         <p className={`${styles["cardItemPrice"]}`}>${this.props.cardPrice}</p>
+
+        {this.state.isHover ? (
+          <div onClick={addToCardHandler} className={`${styles["addToCartWrapper"]}`}>
+            <img src={addToCartLogo} alt="addToCart" />
+          </div>
+        ) : null}
       </div>
     );
   }
